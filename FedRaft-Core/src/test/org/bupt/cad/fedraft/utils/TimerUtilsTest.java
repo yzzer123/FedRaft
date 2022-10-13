7f0032af0957ecfdf89be6674e8e603721613123
yzzer123 <13055686620@139.com>
2022-10-13 15:27:51 +0800

    使用新的高性能timer计时器

---
+++ FedRaft-Core/src/test/org/bupt/cad/fedraft/utils/TimerUtilsTest.java
@@ -0,0 +1,95 @@
+package org.bupt.cad.fedraft.utils;
+
+
+import com.ifesdjeen.timer.HashedWheelTimer;
+import com.ifesdjeen.timer.WaitStrategy;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bupt.cad.fedraft.server.FedRaftServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+
+public class TimerUtilsTest {
+
+
+    private static final Logger logger = LogManager.getLogger(FedRaftServer.class);
+
+    private HashedWheelTimer timer;
+    private final CountDownLatch finished = new CountDownLatch(1);
+
+
+    @Before
+    public void setUp() throws Exception {
+//        timer = new HashedWheelTimer(5, TimeUnit.MILLISECONDS);
+        timer = new HashedWheelTimer("timer", TimeUnit.MICROSECONDS.toNanos(10), 10
+                , new WaitStrategy.BusySpinWait()
+                , Executors.newFixedThreadPool(5));
+//        timer
+//        timer.start();
+    }
+
+
+    @Test
+    public void addTimeoutTask() {
+
+        ScheduledFuture<?> timeout = timer.schedule(new Runnable() {
+            @Override
+            public void run() {
+                logger.info("timeout");
+                finished.countDown();
+            }
+        }, 100, TimeUnit.MILLISECONDS);
+        logger.info("begin tick");
+
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            timeout.cancel(true);
+            finished.countDown();
+        }
+
+    }
+
+
+    @Test
+    public void addScheduledTask() {
+
+        ScheduledFuture<?> timeout = timer.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                logger.info("done");
+            }
+        }, 0, 100, TimeUnit.MILLISECONDS);
+        logger.info("begin tick");
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            timeout.cancel(true);
+            finished.countDown();
+        }
+
+        logger.info("begin tick");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        finished.await();
+//        timer.stop();
+        timer.shutdown();
+    }
+
+}
